38c1029c30f2c8c729698516bf93de5abfa9ddae
Maksim Goncharenko <gm79@list.ru>
2020-02-03 00:00:56 +0300

    limitation test for hosts implemented

--- cmd/tests/tests.go
+++ cmd/tests/tests.go
@@ -9,11 +9,12 @@ import (
 	"github.com/gmax79/bfservice/internal/grpccon"
 )
 
-const timeout = time.Second * 5
+const timeout = time.Second * 10
 
 var tests = []func(*grpccon.Client) error{
 	testHealthCheck,
-	testLimitationLogin,
+	testLimitationLoginPassword,
+	testLimitationHost,
 	//testAddWhiteList,
 	//testWhiteLists,
 }
@@ -50,7 +51,7 @@ func check(conn *grpccon.Client, logins, passwords, ipaddr func() string) *check
 		if resp.Status {
 			result.logins[login]++
 			result.passwords[password]++
-			result.hosts[host]++
+			result.hosts[ip]++
 		}
 	}
 	return &result
@@ -69,9 +70,12 @@ func testHealthCheck(conn *grpccon.Client) error {
 	return conn.HealthCheck(ctx)
 }
 
-func testLimitationLogin(conn *grpccon.Client) (err error) {
-	reset(conn, "login", "192.168.1.1") // reset blocks for test's login and ip (to repeating tests)
-	randomPassword := randomString()    // use random password, exclude conflicts after restart test (blocking by password)
+func testLimitationLoginPassword(conn *grpccon.Client) error {
+	err := reset(conn, "login", "192.168.1.1") // reset blocks for test's login and ip (to repeating tests)
+	if err != nil {
+		return err
+	}
+	randomPassword := randomString() // use random password, exclude conflicts after restart test (blocking by password)
 	logins := stringGenerator(150, 50, "login")
 	passwords := fromConstGenerator(randomPassword, 200)
 	ip := fromConstGenerator("192.16.1.1", 200)
@@ -83,12 +87,36 @@ func testLimitationLogin(conn *grpccon.Client) (err error) {
 	if err != nil {
 		return err
 	}
-	fmt.Printf("limits as settings, calls %d, passed login 'login': %d, password '%s': %d\n",
-		res.calls, res.logins["login"], randomPassword, res.passwords[randomPassword])
 	testLoginsRate := res.logins["login"]
 	testPasswordRate := res.passwords[randomPassword]
+	fmt.Printf("limits as settings, calls %d, passed login 'login': %d, password '%s': %d\n",
+		res.calls, testLoginsRate, randomPassword, testPasswordRate)
 	if rates.LoginRate != testLoginsRate || rates.PasswordRate != testPasswordRate {
-		return errors.New("test rates not equal. server not restarted and test parameters already blocked?")
+		return errors.New("testLimitationLoginPassword failed")
+	}
+	return res.err
+}
+
+func testLimitationHost(conn *grpccon.Client) error {
+	host := "192.168.2.1"
+	err := reset(conn, "", host)
+	if err != nil {
+		return err
+	}
+	passwords := randomString
+	logins := randomString
+	ip := ipGenerator(300, "192.168.2.0", 1100, host)
+	res := check(conn, logins, passwords, ip)
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+	rates, err := conn.GetState(ctx)
+	if err != nil {
+		return err
+	}
+	testHostRate := res.hosts[host]
+	fmt.Printf("limits as settings, calls %d, passed ip '%s': %d\n", res.calls, host, testHostRate)
+	if rates.HostRate != testHostRate {
+		return errors.New("testLimitationHost failed")
 	}
 	return res.err
 }
