9c940d240d11ace523d4f56b6bc1b69835c782fa
Maksim Goncharenko <gm79@list.ru>
2020-02-05 18:16:10 +0300

    storage of redis in progress 2

--- internal/storage/listprov.go
+++ internal/storage/listprov.go
@@ -1,9 +1,11 @@
 package storage
 
+// StringIterator - interface to iterate over list
+type StringIterator func() (string, bool)
+
 // ListProvider - interface to working with list in external storage
 type ListProvider interface {
 	Add(item string) (bool, error)
 	Delete(item string) (bool, error)
-	Count() (int, error)
-	Get(index int) (string, error)
+	Iterator() (StringIterator, error)
 }
--- internal/storage/redis.go
+++ internal/storage/redis.go
@@ -4,26 +4,39 @@ import "github.com/go-redis/redis/v7"
 
 type redisListProvider struct {
 	rc *redis.Client
+	id string
 }
 
 func createRedisListProvider(rc *redis.Client, id string) (*redisListProvider, error) {
 	var p redisListProvider
 	p.rc = rc
+	p.id = id
 	return &p, nil
 }
 
 func (p redisListProvider) Add(item string) (bool, error) {
-	p.rc.LInsert()
+	result := p.rc.SAdd(p.id, item)
+	v, err := result.Val(), result.Err()
+	flag := v == 0
+	return flag, err
 }
 
 func (p redisListProvider) Delete(item string) (bool, error) {
-
+	result := p.rc.SRem(p.id, item)
+	v, err := result.Val(), result.Err()
+	flag := v == 0
+	return flag, err
 }
 
-func (p redisListProvider) Count() (int, error) {
-
-}
-
-func (p redisListProvider) Get(index int) (string, error) {
-
+func (p redisListProvider) Iterator() (StringIterator, error) {
+	cmd := p.rc.SScan(p.id, 0, "", 0)
+	err := cmd.Err()
+	if err != nil {
+		return nil, err
+	}
+	iterator := func() (string, bool) {
+		val := cmd.String()
+		return val, cmd.Err() != nil
+	}
+	return iterator, nil
 }
