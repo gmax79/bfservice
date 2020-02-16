5cf2af5b35f9208f54a2f10c6ddb1fce3d66e751
Maksim Goncharenko <gm79@list.ru>
2020-02-16 14:00:04 +0300

    downgrade protobuf

--- build/ci/install_protoc.sh
+++ build/ci/install_protoc.sh
@@ -3,15 +3,17 @@
 apt-get update -y
 apt-get install unzip
 
-PROTOC_VERSION="3.11.4"
+PROTOC_VERSION="3.9.1"
 PROTOC_ZIP="protoc-$PROTOC_VERSION-linux-x86_64.zip"
 curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v$PROTOC_VERSION/$PROTOC_ZIP
 unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
 unzip -o $PROTOC_ZIP -d /usr/local 'include/*'
 rm -f $PROTOC_ZIP
 
-GIT_TAG="v1.2.0" # protoc-gen-go version
-go get -d -u github.com/golang/protobuf/protoc-gen-go
-mkdir -p "$(go env GOPATH)"/src/github.com/golang/protobuf
-git -C "$(go env GOPATH)"/src/github.com/golang/protobuf checkout $GIT_TAG
-go install github.com/golang/protobuf/protoc-gen-go
+go get -u github.com/golang/protobuf/protoc-gen-go
+
+#GIT_TAG="v1.2.0" # protoc-gen-go version
+#go get -d -u github.com/golang/protobuf/protoc-gen-go
+#mkdir -p "$(go env GOPATH)"/src/github.com/golang/protobuf
+#git -C "$(go env GOPATH)"/src/github.com/golang/protobuf checkout $GIT_TAG
+#go install github.com/golang/protobuf/protoc-gen-go
