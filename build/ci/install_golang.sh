57cf540b9306e835dc277e55604c3617dbaa5611
Maksim Goncharenko <gm79@list.ru>
2020-02-16 22:25:54 +0300

    try set PATH for go 2

--- build/ci/install_golang.sh
+++ build/ci/install_golang.sh
@@ -4,4 +4,4 @@ GOLANG_VERSION="1.13.7"
 
 curl -OL https://dl.google.com/go/go$GOLANG_VERSION.linux-amd64.tar.gz
 tar -C /usr/local -xzf go$GOLANG_VERSION.linux-amd64.tar.gz
-export PATH=/usr/local/go:$PATH
+
